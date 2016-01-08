8273a72c3b2b860dbdd74ceb7f033de9286629c7
牧野幸太郎 <mk.taro0628@gmail.com>
2016-01-09 07:03:13 +0900

    微調整

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ function initialize() {
     var latlng = new google.maps.LatLng(34.698069, 135.503788);
     
     var myOptions = {
-        zoom: 18, /*拡大比率*/
+        zoom: 17, /*拡大比率*/
         center: latlng, /*表示枠内の中心点*/
         mapTypeId: google.maps.MapTypeId.ROADMAP/*表示タイプの指定*/
     };
@@ -12,4 +12,38 @@ function initialize() {
         position: latlng,
         map: map
     });
+    
+    var style = [
+      {
+        "featureType": "poi",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },{
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          { "visibility": "off" }
+        ]
+      },{
+        "featureType": "road.highway",
+        "stylers": [
+          { "saturation": -50 },
+          { "lightness": 51 }
+        ]
+      },{
+        "stylers": [
+          { "saturation": -100 },
+          { "gamma": 0.71 }
+        ]
+      }
+    ]
+    
+    var styleOptions = {
+        name: "シンプル"
+    };
+  
+    var MapType = new google.maps.StyledMapType(style, styleOptions);
+    map.mapTypes.set('simple', MapType);
+    map.setMapTypeId('simple');
 }
